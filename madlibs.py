8a1928dfdc7e38f9e60d27626acf0147578d648d
Lihini Nisansala <lihininisansala95@gmail.com>
2021-02-20 23:44:53 +0530

    Add madlibs code

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,17 @@
+programming = "python"
+
+# string concatenation methods
+print("Hey I do " + programming)
+print("Hey I do {}".format(programming))
+print(f"Hey I do {programming}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
 
